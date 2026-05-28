e6debb5a9def4989cf469727a79b98dd67803bec
Tanguy <tanguy_job@hotmail.fr>
2026-05-28 19:01:58 +0700

    first commit + first database update

---
+++ BulkyBookWeb/Migrations/20260528101533_modifycategoryToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyBookWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class modifycategoryToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Name { get; set; } = string.Empty;
+    }
+}
