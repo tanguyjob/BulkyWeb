54d1f0573c28aaccfd813f44e57e7606081e9962
Tanguy <tanguy_job@hotmail.fr>
2026-05-28 19:24:37 +0700

    seed category

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,16 @@ namespace BulkyBookWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action"},
+                new Category { Id = 2, Name = "SciFi"},
+                new Category { Id = 3, Name = "History"}
+            );
+        }
     }
 }
